d4c2a86ed4f321986caf0ba3535d7ba2bcf334bf
bjoffficial <50042488+bjoffficial@users.noreply.github.com>
2019-07-14 16:04:41 +0530

    Update and rename input and output to input_and_output.java

--- input_and_output.java
+++ input_and_output.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+
+/*************************************
+author: balaji
+*************************************/
+
 public class Solution {
 
     public static void main(String[] args) {
